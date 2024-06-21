27a498c85274a87ccbc8ff5d022db18998dce539
Varun Bhardwaj <bhardwajvarundev@gmail.com>
2024-06-21 20:30:39 +0530

    fix view path

--- src/main/java/org/varun/lc/config/LoveCalculatorApplicationConfig.java
+++ src/main/java/org/varun/lc/config/LoveCalculatorApplicationConfig.java
@@ -3,8 +3,10 @@ package org.varun.lc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "org.varun.lc.controllers")
 public class LoveCalculatorApplicationConfig {
@@ -12,7 +14,7 @@ public class LoveCalculatorApplicationConfig {
     @Bean
     InternalResourceViewResolver viewResolver(){
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
