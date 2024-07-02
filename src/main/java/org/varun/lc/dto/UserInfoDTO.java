5300c2d4a3b5f27c8c50fdbcedaf7e9e4126a502
Varun Bhardwaj <bhardwajvarundev@gmail.com>
2024-07-02 08:09:34 +0530

    Server side error handling

--- src/main/java/org/varun/lc/dto/UserInfoDTO.java
+++ src/main/java/org/varun/lc/dto/UserInfoDTO.java
@@ -1,8 +1,19 @@
 package org.varun.lc.dto;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class UserInfoDTO {
-    String userName="Mr kukur";
-    String crushName="Miss chutki";
+    @NotBlank(message = "* user name can not be blank")
+    @Size(min = 3, message = "* username should have at least 3 characters")
+    String userName;
+    @NotBlank(message = "* user name can not be blank")
+    @Size(min = 3, message = "* username should have at least 3 characters")
+    String crushName;
+
+    @AssertTrue(message = "* Should accept terms and conditions.")
+    boolean termsAndConditions;
 
     public String getUserName() {
         return userName;
@@ -20,6 +31,13 @@ public class UserInfoDTO {
         this.crushName = crushName;
     }
 
+    public boolean isTermsAndConditions() {
+        return termsAndConditions;
+    }
+
+    public void setTermsAndConditions(boolean termsAndConditions) {
+        this.termsAndConditions = termsAndConditions;
+    }
     @Override
     public String toString() {
         return "UserInfo{" +
---
+++ src/main/java/org/varun/lc/dto/UserRegistrationDTO.java
@@ -0,0 +1,60 @@
+package org.varun.lc.dto;
+
+import java.util.Arrays;
+
+public class UserRegistrationDTO {
+    private String name="Kukurwa";
+    private String userName="beanwa";
+    private char[] password;
+    private String country;
+    private String[] hobbies;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
