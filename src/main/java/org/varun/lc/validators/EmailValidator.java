173483207c7ff12e279eb0a530e0a3468354d17a
Varun Bhardwaj <bhardwajvarundev@gmail.com>
2024-07-03 17:52:58 +0530

    Email validator using spring validor

---
+++ src/main/java/org/varun/lc/validators/EmailValidator.java
@@ -0,0 +1,27 @@
+package org.varun.lc.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import org.varun.lc.dto.CommunicationDto;
+import org.varun.lc.dto.UserRegistrationDTO;
+
+public class EmailValidator implements Validator {
+    //check if the object matches the dto object
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserRegistrationDTO.class.equals(clazz);
+    }
+
+    //only called if supports() return true
+    @Override
+    public void validate(Object object, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "communicationDto.email", "communicationDto.email.required", "Email cannot be empty");
+        UserRegistrationDTO userRegistrationDTO = (UserRegistrationDTO) object;
+        CommunicationDto communicationDto = userRegistrationDTO.getCommunicationDto();
+        String email = communicationDto.getEmail();
+        if (!email.endsWith("@seleniumexpress.com")) {
+            errors.rejectValue("communicationDto.email", "communicationDto.email.invalid", "Email must end with '@seleniumexpress.com'");
+        }
+    }
+}
